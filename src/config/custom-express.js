3edf837b9b339ad2a086cb51149dda31657b577f
vinifiore <araujovinicius1@hotmail.com>
2022-09-16 23:13:12 -0300

    Corrigido erro de cors

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const rotasCadAuth = require('../app/rotas/signin-signup');
-const servicoEmpresa = require('../app/rotas/sevicos-empresa');
-const reservaCliente = require('../app/rotas/reservas-cliente');
+const rotasCadAuth = require("../app/rotas/signin-signup");
+const servicoEmpresa = require("../app/rotas/sevicos-empresa");
+const reservaCliente = require("../app/rotas/reservas-cliente");
 
 //Rptas de cadastro e autenticação cliente/empresa
 rotasCadAuth(app);
