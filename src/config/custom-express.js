e235b6e4c9bb0af70ea736523faa6a8de9a535ac
vinifiore <araujovinicius1@hotmail.com>
2022-09-16 23:22:26 -0300

    feat atualização do cors

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var cors = require("cors");
 
 const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
